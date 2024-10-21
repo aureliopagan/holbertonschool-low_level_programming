1c77d490411840d87d6bac170786d15135dc98e2
aureliopagan <aureliopagan6@gmail.com>
2024-10-21 10:17:01 -0400

    m

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-echo -e '#include <stdio.h>\nint main() { puts("Programming is like building a multilingual puzzle"); return 0; }' > puzzle.c
+#include <stdio.h>
+/**
+ * main - start the program
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
