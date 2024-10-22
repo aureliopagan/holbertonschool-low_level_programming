4819b532036d20a92e3e0cec9df4bdb11e9c7c8d
aureliopagan <aureliopagan6@gmail.com>
2024-10-22 12:14:17 -0400

    m

---
+++ functions_nested_loops/11-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_to_98(0);
+    print_to_98(102);
+    print_to_98(98);
+    return (0);
+}
+
