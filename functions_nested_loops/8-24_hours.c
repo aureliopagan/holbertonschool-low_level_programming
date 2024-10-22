dfbd3371a6a747301516fe8d3b436e2e67e72cd0
aureliopagan <aureliopagan6@gmail.com>
2024-10-22 11:17:32 -0400

    m

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+    int hour;
+    int minute;
+
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar((hour / 10) + '0');  /* Print the tens place of hour */
+            _putchar((hour % 10) + '0');  /* Print the units place of hour */
+            _putchar(':');                 /* Print the colon */
+            _putchar((minute / 10) + '0'); /* Print the tens place of minute */
+            _putchar((minute % 10) + '0'); /* Print the units place of minute */
+            _putchar('\n');                /* New line */
+        }
+    }
+}
+
---
+++ functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    jack_bauer();  /* Call the function to print every minute */
+    return (0);
+}
+
