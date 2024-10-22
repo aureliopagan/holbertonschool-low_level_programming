cc8b7262ec626ca324ef836f5d6986f5f9c4c606
aureliopagan <aureliopagan6@gmail.com>
2024-10-22 11:24:43 -0400

    m

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,23 +2,26 @@
 
 /**
  * jack_bauer - prints every minute of the day from 00:00 to 23:59
+ *
+ * This function loops through each hour and minute of the day,
+ * printing the time in the format HH:MM.
  */
 void jack_bauer(void)
 {
-    int hour;
-    int minute;
+	int hour;
+	int minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar((hour / 10) + '0');  /* Print the tens place of hour */
-            _putchar((hour % 10) + '0');  /* Print the units place of hour */
-            _putchar(':');                 /* Print the colon */
-            _putchar((minute / 10) + '0'); /* Print the tens place of minute */
-            _putchar((minute % 10) + '0'); /* Print the units place of minute */
-            _putchar('\n');                /* New line */
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');  /* Print the tens place of hour */
+			_putchar((hour % 10) + '0');  /* Print the units place of hour */
+			_putchar(':');                 /* Print the colon */
+			_putchar((minute / 10) + '0'); /* Print the tens place of minute */
+			_putchar((minute % 10) + '0'); /* Print the units place of minute */
+			_putchar('\n');                /* New line */
+		}
+	}
 }
 
