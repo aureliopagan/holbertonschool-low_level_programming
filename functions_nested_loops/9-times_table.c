8fb5d5918a3750c2f7601d268ab26c1f48c11734
aureliopagan <aureliopagan6@gmail.com>
2024-10-22 11:30:43 -0400

    m

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+
+			if (j == 0)
+			{
+				_putchar(product + '0');  /* Print the first number without comma */
+			}
+			else
+			{
+				_putchar(',');              /* Print a comma before the next number */
+				_putchar(' ');              /* Print space after the comma */
+
+				if (product < 10)
+				{
+					_putchar(' ');          /* Print a space for single-digit products */
+				}
+				_putchar(product + '0');     /* Print the product */
+			}
+		}
+		_putchar('\n');                  /* New line after each row */
+	}
+}
+
