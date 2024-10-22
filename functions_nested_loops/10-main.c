35966251063bcdbc671382053e2491e4244867e2
aureliopagan <aureliopagan6@gmail.com>
2024-10-22 12:05:10 -0400

    m

---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int result;
+
+    result = add(5, 3);
+    printf("The sum of 5 and 3 is: %d\n", result);
+    return (0);
+}
+
