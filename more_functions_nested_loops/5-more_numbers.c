4865f4eb613a443384e8fa3726fd79571f79d6a5
aureliopagan <aureliopagan6@gmail.com>
2024-10-28 12:17:05 -0400

    From 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the numbers from 0 to 14, ten times.
- */
+ * more_numbers - prints 10 times the numbers from 0 to 14 
+**/
+
+
 void more_numbers(void)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; i < 10; i++)  /* Repeat 10 times */
-	{
-		for (j = 0; j <= 14; j++)  /* Print numbers from 0 to 14 */
-		{
-			if (j > 9)  /* If the number is two digits */
-			{
-				_putchar('1');  /* Print the first digit */
-				_putchar((j % 10) + '0');  /* Print the second digit */
-			}
-			else
-			{
-				_putchar(j + '0');  /* Print single digit */
-			}
-		}
-	}
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j <= 14; j++)
+                {
+                        if (j >= 10)
+                                _putchar('1');
+                        _putchar((j % 10) + '0');
+                }
+                _putchar('\n');
+        }
 }
-
