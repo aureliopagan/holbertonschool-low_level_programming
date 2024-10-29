1592c2ffe60b702ac149098c7e930f389a0a1687
aureliopagan <aureliopagan6@gmail.com>
2024-10-29 11:06:47 -0400

    Printing diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -21,9 +21,9 @@ void print_diagonal(int n)
     {
         for (j = 0; j < i; j++)
         {
-            _putchar(' ');
+            _putchar('-');
         }
-        _putchar('\');
+        _putchar('\\');
         _putchar('\n');
     }
 }
