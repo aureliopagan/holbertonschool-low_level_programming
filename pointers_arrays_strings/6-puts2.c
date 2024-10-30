db4dab41556bef359234d1c88b27edc4e473c7fc
aureliopagan <aureliopagan6@gmail.com>
2024-10-30 12:04:59 -0400

    Learning pointers and arrays

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,22 @@
 #include "main.h"
+
 /**
- * puts2 - function name 
- *
- *@str: variable that points where to print
+ * puts2 - prints every other character of a string.
+ * @str: pointer to the string to print.
  *
  * Return: void
- **/
-
+ */
 void puts2(char *str)
 {
 	int len = 0, i;
 
-	if (len != 8)
-	{
-		while (str[len] != '\0')
+	while (str[len] != '\0')  /* Calculate the length of the string */
 		len++;
 
-		for (i = 0; i < len; i += 2)
-		{
-			_putchar(str[i]);
-		}
+	for (i = 0; i < len; i += 2)  /* Print every other character */
+	{
+		_putchar(str[i]);
 	}
-	_putchar('\n');
+	_putchar('\n');  /* Print a new line at the end */
 }
+
