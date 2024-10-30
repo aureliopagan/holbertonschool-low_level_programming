ac5030a33acc011265b70fa85ad1633e3578e7c5
aureliopagan <aureliopagan6@gmail.com>
2024-10-30 14:19:48 -0400

    jaja

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,12 +2,12 @@
 
 /**
  * _strncpy - Copies a string
- * @dest: destination
- * @src: source
- * @n: maximum number variable 
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to copy from src
  *
  * Return: A pointer to the resulting string dest
-**/
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
