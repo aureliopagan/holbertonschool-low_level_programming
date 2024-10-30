a64d063b61314eafbd042f8d28f0947c992cb02a
aureliopagan <aureliopagan6@gmail.com>
2024-10-30 14:44:24 -0400

    fixing :(

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,22 +8,22 @@
  */
 char *leet(char *str)
 {
-	char *leet_chars = "43071";
-	char *normal_chars = "aeotlAEOTL";
-	int i, j;
+    char *leet_chars = "43071";
+    char *normal_chars = "aeotlAEOTL";
+    int i, j;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (str[i] == normal_chars[j])
-			{
-				str[i] = leet_chars[j / 2];  /* Use j / 2 to access leet_chars */
-				break;
-			}
-		}
-	}
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (str[i] == normal_chars[j])
+            {
+                str[i] = leet_chars[j / 2];  /* Use j / 2 to access leet_chars */
+                break;
+            }
+        }
+    }
 
-	return (str);
+    return (str);
 }
 
