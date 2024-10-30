eb081ebbea4322ad17b69967a78da1c0b05f66d1
aureliopagan <aureliopagan6@gmail.com>
2024-10-30 14:47:57 -0400

    fixed :)

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,22 +8,22 @@
  */
 char *leet(char *str)
 {
-    char *leet_chars = "43071";
-    char *normal_chars = "aeotlAEOTL";
-    int i, j;
+	char *leet_chars = "43071"; /* Characters to replace with */
+	char *normal_chars = "aeotlAEOTL"; /* Characters to be replaced */
+	int i, j;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (str[i] == normal_chars[j])
-            {
-                str[i] = leet_chars[j / 2];  /* Use j / 2 to access leet_chars */
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == normal_chars[j])
+			{
+				str[i] = leet_chars[j / 2]; /* Replace the character */
+				break;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
 
