732cde2a2812afd50e260fa242b18a20d6bdaa8e
aureliopagan <aureliopagan6@gmail.com>
2024-11-22 06:22:14 -0400

    m

--- argc_argv/0-main.c
+++ argc_argv/0-main.c
@@ -2,16 +2,11 @@
 
 /**
  * main - Entry point of the program
- * @argc: Argument count
- * @argv: Argument vector
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	(void)argc;
-
 	printf("%s\n", argv[0]);
 
 	return (0);
