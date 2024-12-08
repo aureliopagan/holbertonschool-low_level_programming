38e3f813f73f8a305f68568f47e23de5c5d06d4d
aureliopagan <aureliopagan6@gmail.com>
2024-12-08 07:49:01 -0400

    making task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: The name of the file to append to.
+ * @text_content: The NULL-terminated string to append to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ *	If filename is NULL, return -1.
+ *	If text_content is NULL, do not add anything, return 1.
+ *	If the file does not exist or cannot be written to, return -1.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
