74aeb19a81e465b38769e2aa28fe1e2db3deebc5
aureliopagan <aurepgst@gmail.com>
2025-03-24 21:54:38 -0400

    Task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -34,7 +34,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		}
 
-		/* If a separator is provided and it's not the last string, print
+		/* If a separator is provided and it's not the last string, print 
 		 * the separator.
 		 */
 		if (separator != NULL && i < n - 1)
